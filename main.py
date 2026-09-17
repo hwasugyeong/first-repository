d469cf9ebc4afa35199dcfd76712cb6c61dac417
hwasugyeong <hwasu2006@naver.com>
2026-09-17 13:59:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
     pn.is_prime_number(24)
+<<<<<<< HEAD
     pn.is_prime_number(31)
+=======
+    pn.is_prime_number(31)
+  
+>>>>>>> 962e7906cb5394cd61fcedcb8bcb54b2b8beb401
